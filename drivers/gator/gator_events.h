58582498af38e19c8f4eb911700ee997213fe5e9
yEsPaP <your_email@your_domain.com>
2019-02-21 11:55:32 +0000

    fix makefile

---
+++ drivers/gator/gator_events.h
@@ -0,0 +1,9 @@
+/* This file is auto generated */
+
+__weak int gator_events_mali_init(void);
+__weak int gator_events_mmaped_init(void);
+
+static int (*gator_events_list[])(void) = {
+	gator_events_mali_init,
+	gator_events_mmaped_init,
+};
